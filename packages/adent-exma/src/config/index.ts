7dcb316a902479c4ff0ff3b695dd30cf24bd516a
Chris Blanquera <chris@incept.asia>
2024-03-24 22:07:53 +0800

    adding metadata config and generic helpers

---
+++ packages/adent-exma/src/config/index.ts
@@ -0,0 +1,106 @@
+// This a geeral component map used to configure fields, validators, and 
+// formats defined inside a schema.exma file.
+import fields from './fields.json';
+import formats from './formats.json';
+
+// These are the possible fields that can be defined 
+// in schema.exma ie. `@field.text`
+export type FieldMethod = 'active' 
+  | 'autocomplete' | 'checkbox'  | 'checklist' 
+  | 'code'         | 'color'     | 'country' 
+  | 'created'      | 'currency'  | 'date'
+  | 'datetime'     | 'email'     | 'fieldset'
+  | 'file'         | 'filelist'  | 'image'
+  | 'imagelist'    | 'input'     | 'integer'
+  | 'json'         | 'mask'      | 'metadata'
+  | 'none'         | 'number'    | 'password'
+  | 'phone'        | 'price'     | 'radio'        
+  | 'radiolist'    | 'range'     | 'rating'
+  | 'select'       | 'slider'    | 'slug'
+  | 'small'        | 'switch'    | 'table'
+  | 'tags'         | 'text'      | 'textarea'
+  | 'textlist'     | 'time'      | 'updated'
+  | 'url'          | 'wysiwyg';
+
+// These are the possible validators that can be defined 
+// in schema.exma ie. `@is.required`
+export type ValidatorMethod = 'eq'
+  | 'ne'       | 'notempty' | 'option'
+  | 'required' | 'regex'    | 'date'
+  | 'datetime' | 'time'     | 'future'
+  | 'past'     | 'present'  | 'gt'
+  | 'ge'       | 'lt'       | 'le'
+  | 'float'    | 'integer'  | 'number'
+  | 'price'    | 'ceq'      | 'cgt'
+  | 'cge'      | 'clt'      | 'cle'
+  | 'wgt'      | 'wge'      | 'wlt'
+  | 'wle'      | 'cc'       | 'email'
+  | 'hex'      | 'color'    | 'url';
+
+// These are the possible formatters that can be defined
+// in schema.exma `@list.link({ target '_blank'})`
+export type FormatMethod = 'captal' 
+  | 'char'     | 'color'    | 'comma'
+  | 'country'  | 'currency' | 'date' 
+  | 'carousel' | 'email'    | 'escaped'  
+  | 'formula'  | 'hide'     | 'html'     
+  | 'image'    | 'json'     | 'line'  
+  | 'link'     | 'list'     | 'lower'
+  | 'markdown' | 'metadata' | 'none'
+  | 'number'   | 'ol'       | 'pretty' 
+  | 'price'    | 'phone'    | 'rating'  
+  | 'rel'      | 'relative' | 'space' 
+  | 'table'    | 'tags'     | 'text'  
+  | 'ul'       | 'upper'    | 'word' 
+  | 'yesno';
+
+//column options
+
+// This is the output format for the @field.text, @list.text options from a 
+// column in a model in schema.exma
+export type ColumnOption = { 
+  component: string|false, 
+  attributes: Record<string, any>
+};
+
+// This is a list of all defined fields, validators,  
+// and formats this code will understand
+const config = {
+  fields: fields as Record<string, ColumnOption>,
+  formats: formats as Record<string, ColumnOption>,
+  validators: [
+    'eq',     'ne',       'notempty',
+    'option', 'required', 'regex',
+    'date',   'datetime', 'time',
+    'future', 'past',     'present',
+    'gt',     'ge',       'lt',
+    'le',     'float',    'integer',
+    'number', 'price',    'ceq',
+    'cgt',    'cge',      'clt',
+    'cle',    'wgt',      'wge',
+    'wlt',    'wle',      'cc',
+    'email',  'hex',      'color',
+    'url'
+  ]
+};
+
+export const types: Record<string, string> = {
+  String: 'string',
+  Number: 'number',
+  Int: 'integer',
+  Integer: 'integer',
+  Float: 'float',
+  Boolean: 'boolean',
+  Date: 'date',
+  Datetime: 'datetime',
+  Time: 'time',
+  Json: 'json',
+  Hash: 'json'
+};
+
+export const spanable = [ 
+  'number', 'integer', 'float', 
+  'date',   'time',    'datetime' 
+];
+
+export default config;
---
+++ packages/adent-exma/src/helpers.ts
@@ -0,0 +1,45 @@
+export function capitalize(word: string) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+export function lowerlize(word: string) {
+  return word.charAt(0).toLowerCase() + word.slice(1);
+}
+
+export function formatCode(code: string): string {
+  code = code
+    .replace(/\}\s+else\s+if\s+\(/g, '} else if (')
+    .replace(/\s*\n\s*\n\s*/g, "\n")
+    .trim();
+  const lines = code.split("\n");
+  let indent = 0;
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+    if (line.match(/^\}/g) || line.match(/^\)/g)) {
+      indent -= 2;
+    }
+    lines[i] = `${' '.repeat(indent)}${line}`;
+    if (line.match(/\s*\{\s*$/g) || line.match(/\s*\(\s*$/g)) {
+      indent += 2;
+    }
+  }
+  return lines.join("\n");
+};
+
+export function camelize(str: string) {
+  return lowerlize(
+    str.trim()
+      //replace special characters with underscores
+      .replace(/[^a-zA-Z0-9]/g, '_')
+      //replace multiple underscores with a single underscore
+      .replace(/_{2,}/g, '_')
+      //trim underscores from the beginning and end of the string
+      .replace(/^_+|_+$/g, '')
+      //replace underscores with capital
+      .replace(/([-_][a-z0-9])/ig, ($1) => {
+        return $1.toUpperCase()
+          .replace('-', '')
+          .replace('_', '');
+      })
+  );
+}
